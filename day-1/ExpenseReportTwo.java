8baf9d9ae25564b6378d0f9b793a5dcb51abc151
Zak Booth <zak.a.booth@gmail.com>
2020-12-03 00:46:19 +0800

    Day 1 complete!

---
+++ day-1/ExpenseReportTwo.java
@@ -0,0 +1,49 @@
+import java.util.*;
+import java.io.*;
+
+public class ExpenseReportTwo
+{
+	public static void main(String[] args)
+	{
+		final int SUM = 2020;
+		int[] expenses = new int[200];
+
+		readFileToIntArray("report.txt", expenses);
+
+		for (int i = 0; i < expenses.length; i++)
+		{
+			for (int j = i + 1; j < expenses.length; j++)
+			{
+				for (int k = j + 1; k < expenses.length; k++)
+				{
+					if (expenses[i] + expenses[j] + expenses[k] == SUM)
+					{
+						System.out.println(expenses[i] * expenses[j] * expenses[k]);
+					}
+				}
+			}
+		}
+	}
+
+	private static void readFileToIntArray(String filename, int[] array)
+	{
+		try
+		{
+			File file = new File(filename);
+			Scanner reader = new Scanner(file);
+			int lineNum = 0;
+
+			while (reader.hasNextLine())
+			{
+				array[lineNum] = Integer.parseInt(reader.nextLine());
+				lineNum++;
+			}
+
+			reader.close();
+		}
+		catch (IOException e)
+		{
+			System.out.println("Error occurred!");
+		}
+	}
+}
